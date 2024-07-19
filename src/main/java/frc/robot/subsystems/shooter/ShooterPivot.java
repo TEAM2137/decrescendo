d893654393ddef18374471edad9214be5a79de1c
[Error_27] <error27dev@gmail.com>
2024-07-18 22:49:04 -0400

    Create shooterpivot classes

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterPivot.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterPivot extends SubsystemBase {
+    private final ShooterPivotIOInputsAutoLogged inputs = new ShooterPivotIOInputsAutoLogged();
+    private final ShooterPivotIO io;
+
+    public ShooterPivot(ShooterPivotIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+    }
+
+    public Command setVoltage(double volts) {
+        return runOnce(() -> io.setVoltage(volts));
+    }
+
+    public Command setPositionTarget(Rotation2d target) {
+        return runOnce(() -> io.setPositionTarget(target));
+    }
+
+    public Command setBrakeMode(boolean brake) {
+        return runOnce(() -> io.setBrakeMode(brake));
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterPivotIO.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface ShooterPivotIO {
+    @AutoLog
+    public class ShooterPivotIOInputs {
+        public Rotation2d absoluteEncoderPosition =  new Rotation2d();
+        public Rotation2d motorEncoderPosition = new Rotation2d();
+        public double velocityRadPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+
+    public default void updateInputs(ShooterPivotIOInputs inputs) {}
+
+    public default void setVoltage(double volts) {}
+
+    public default void setPositionTarget(Rotation2d target) {}
+
+    public default void setBrakeMode(boolean brake) {}
+}
